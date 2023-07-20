a9aa43d1961fb71b1530996999e4ccab5f1c97aa
IhorPinchuk <pinchukkk@ukr.net>
2023-07-20 19:25:31 +0300

    databaseConnection

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
-// const app = require('./app')
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+import app from './app.js';
 
-// app.listen(3000, () => {
-//   console.log("Server running. Use our API on port: 3000")
-// })
+dotenv.config();
 
-import app from './app.js'
+const {DB_HOST, PORT} = process.env;
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+mongoose.connect(DB_HOST).then(() => {
+  app.listen(PORT, () => {
+    console.log("Database connection successful")
+  })
+}).catch(error => {
+  console.log(error.message);
+process.exit(1);
 })
+
+
 
